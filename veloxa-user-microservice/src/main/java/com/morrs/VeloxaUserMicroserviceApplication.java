ff21849b28e848d4865616f235a8265c786e4c3d
morrs <grishkovea@gmail.com>
2025-05-30 17:36:17 +0300

    init

---
+++ veloxa-user-microservice/src/main/java/com/morrs/VeloxaUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.morrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxaUserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxaUserMicroserviceApplication.class, args);
+	}
+
+}
---
+++ veloxa-user-microservice/src/test/java/com/morrs/VeloxaUserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxaUserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
