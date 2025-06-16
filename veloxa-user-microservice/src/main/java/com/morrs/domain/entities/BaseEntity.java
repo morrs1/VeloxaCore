00285440326f4fff3e53c77497df89d6549bdb9c
morrs <grishkovea@gmail.com>
2025-06-16 20:58:54 +0300

    feat: добавил базовую сущность

---
+++ veloxa-user-microservice/src/main/java/com/morrs/domain/entities/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.morrs.domain.entities;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public abstract class BaseEntity {
+    protected UUID id;
+
+    public BaseEntity(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
