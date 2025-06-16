0a87eb8b82115cbed5e9c97782506174ecb8c9f8
morrs <grishkovea@gmail.com>
2025-06-16 21:03:45 +0300

    feat: добавил handler ошибок

---
+++ veloxa-user-microservice/src/main/java/com/morrs/api/adapters/exception_handlers/ErrorsHandler.java
@@ -0,0 +1,28 @@
+package com.morrs.api.adapters.exception_handlers;
+
+import com.morrs.api.presentation.ErrorResponse;
+import com.morrs.domain.exceptions.BaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@Slf4j
+public class ErrorsHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+        log.warn("{} {}", e.getClass().getSimpleName(), e.getMessage());
+        return ResponseEntity.status(e.getStatusCode())
+                .body(new ErrorResponse(
+                                e.getMessage(),
+                                HttpStatus.valueOf(e.getStatusCode()),
+                                LocalDateTime.now()
+                        )
+                );
+    }
+}
