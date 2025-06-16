cbbd376ffeafff2b277473d54ce746e8c038f4d2
morrs <grishkovea@gmail.com>
2025-06-17 01:19:45 +0300

    fix: поменял User на UserPresentation

--- veloxa-user-microservice/src/main/java/com/morrs/api/adapters/springweb_controllers/FindUserByIdController.java
+++ veloxa-user-microservice/src/main/java/com/morrs/api/adapters/springweb_controllers/FindUserByIdController.java
@@ -1,7 +1,8 @@
 package com.morrs.api.adapters.springweb_controllers;
 
-import com.morrs.api.FindUserByIdApi;
-import com.morrs.domain.entities.User;
+import com.morrs.api.input_ports.FindUserByIdApi;
+import com.morrs.api.mappers.UserMapper;
+import com.morrs.api.presentation.UserPresentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,15 +14,17 @@ import java.util.UUID;
 public class FindUserByIdController {
 
     private final FindUserByIdApi findUserByIdApi;
+    private final UserMapper userMapper;
 
 
-    public FindUserByIdController(FindUserByIdApi findUserByIdApi) {
+    public FindUserByIdController(FindUserByIdApi findUserByIdApi, UserMapper userMapper) {
         this.findUserByIdApi = findUserByIdApi;
+        this.userMapper = userMapper;
     }
 
     @GetMapping("/api/user/{id}")
-    public ResponseEntity<User> findUserById(@PathVariable("id") UUID id) {
-        return ResponseEntity.ok(findUserByIdApi.findUserById(id));
+    public ResponseEntity<UserPresentation> findUserById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(userMapper.toUserPresentation(findUserByIdApi.findUserById(id)));
     }
 
 }
