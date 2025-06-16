5be2dc2f9ef22c64fee0b58f88d459db5d048ae8
morrs <grishkovea@gmail.com>
2025-06-16 21:05:45 +0300

    feat: добавил адаптер output port для нахождения пользователя по id

---
+++ veloxa-user-microservice/src/main/java/com/morrs/spi/adapters/postgres_jdbc/FindUserByIdJdbcTemplateAdapter.java
@@ -0,0 +1,52 @@
+package com.morrs.spi.adapters.postgres_jdbc;
+
+import com.morrs.domain.entities.User;
+import com.morrs.domain.values.user.*;
+import com.morrs.spi.FindUserByIdSpi;
+import com.morrs.spi.exceptions.UserNotFoundException;
+import org.springframework.context.annotation.Scope;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component()
+@Scope("request")
+public class FindUserByIdJdbcTemplateAdapter implements FindUserByIdSpi {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public FindUserByIdJdbcTemplateAdapter(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<User> findUserById(UUID id) {
+        User user;
+        try {
+            user = jdbcTemplate.queryForObject(
+                    "SELECT * FROM users WHERE id = ?",
+                    (rs, rowNum) -> new User(
+                            UUID.fromString(rs.getString("id")),
+                            new Surname(rs.getString("surname")),
+                            new Name(rs.getString("name")),
+                            new Patronymic(rs.getString("patronymic")),
+                            new UserStatus(rs.getString("user_status")),
+                            new NotificationWay(NotificationWayEnum.valueOf(rs.getString("notification_way"))),
+                            new Role(rs.getString("role")),
+                            new Email(rs.getString("email")),
+                            new Password(rs.getString("password")),
+                            new TelegramId(rs.getInt("telegram_id")),
+                            new ProfilePhotoLink(rs.getString("profile_photo_link"))
+                    ),
+                    id
+            );
+        } catch (DataAccessException e) {
+            throw new UserNotFoundException("Could not find user by id: " + id);
+        }
+        return Optional.ofNullable(user);
+    }
+
+}
