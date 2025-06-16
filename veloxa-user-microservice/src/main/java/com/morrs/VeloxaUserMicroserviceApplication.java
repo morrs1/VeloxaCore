3d03577907827390a2b8d5cffe261b55f8f5a7e7
morrs <grishkovea@gmail.com>
2025-06-16 21:07:24 +0300

    setup: зарегистрировал бин usecase в контексте спринга

--- veloxa-user-microservice/src/main/java/com/morrs/VeloxaUserMicroserviceApplication.java
+++ veloxa-user-microservice/src/main/java/com/morrs/VeloxaUserMicroserviceApplication.java
@@ -1,13 +1,22 @@
 package com.morrs;
 
+import com.morrs.spi.adapters.postgres_jdbc.FindUserByIdJdbcTemplateAdapter;
+import com.morrs.usecase.FindUserByIdUseCase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class VeloxaUserMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VeloxaUserMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VeloxaUserMicroserviceApplication.class, args);
+    }
 
+
+    @Bean
+    public FindUserByIdUseCase findUserByIdUseCase(JdbcTemplate jdbcTemplate) {
+        return new FindUserByIdUseCase(new FindUserByIdJdbcTemplateAdapter(jdbcTemplate));
+    }
 }
