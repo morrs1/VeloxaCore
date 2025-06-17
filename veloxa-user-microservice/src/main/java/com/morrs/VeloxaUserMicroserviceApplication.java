bc71f5d694a3c9881bd9e68b339cedc0c8af846e
morrs <grishkovea@gmail.com>
2025-06-17 20:06:37 +0300

    setup: зарегистрировал бин useCase удаления пользователя в контексте спринга

--- veloxa-user-microservice/src/main/java/com/morrs/VeloxaUserMicroserviceApplication.java
+++ veloxa-user-microservice/src/main/java/com/morrs/VeloxaUserMicroserviceApplication.java
@@ -1,9 +1,11 @@
 package com.morrs;
 
 import com.morrs.spi.adapters.postgres_jdbc.CreateUserJdbcTemplateAdapter;
+import com.morrs.spi.adapters.postgres_jdbc.DeleteUserByIdJdbcTemplateAdapter;
 import com.morrs.spi.adapters.postgres_jdbc.FindUserByIdJdbcTemplateAdapter;
 import com.morrs.spi.adapters.postgres_jdbc.UpdateUserInfoJdbcTemplateAdapter;
 import com.morrs.usecase.CreateUserUseCase;
+import com.morrs.usecase.DeleteUserByIdUseCase;
 import com.morrs.usecase.FindUserByIdUseCase;
 import com.morrs.usecase.UpdateUserUseCase;
 import org.springframework.boot.SpringApplication;
@@ -32,5 +34,13 @@ public class VeloxaUserMicroserviceApplication {
     @Bean
     public UpdateUserUseCase updateUserUseCase(JdbcTemplate jdbcTemplate) {
         return new UpdateUserUseCase(new UpdateUserInfoJdbcTemplateAdapter(jdbcTemplate));
+    }
+
+    @Bean
+    public DeleteUserByIdUseCase deleteUserByIdUseCase(JdbcTemplate jdbcTemplate) {
+        return new DeleteUserByIdUseCase(
+                new DeleteUserByIdJdbcTemplateAdapter(jdbcTemplate),
+                new FindUserByIdJdbcTemplateAdapter(jdbcTemplate)
+        );
     }
 }
