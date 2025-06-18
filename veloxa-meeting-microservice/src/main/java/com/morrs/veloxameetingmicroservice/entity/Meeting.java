95cf51db30543380559df29b0bb4625dbc9141dc
morrs <grishkovea@gmail.com>
2025-06-18 21:10:18 +0300

    feat: добавил сущность встречи

---
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/entity/Meeting.java
@@ -0,0 +1,21 @@
+package com.morrs.veloxameetingmicroservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class Meeting {
+
+    UUID id;
+    UUID organizerId;
+    String title;
+    String description;
+    String startTime; //TODO поменять на LocalDateTime
+    String endTime; //TODO поменять на LocalDateTime
+    List<User> participants;
+
+}
