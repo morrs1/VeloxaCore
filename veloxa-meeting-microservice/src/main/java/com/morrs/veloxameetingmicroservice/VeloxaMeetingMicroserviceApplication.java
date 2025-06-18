3a4673d3f4bd9f8e34ad00a808313b303593d69f
morrs <grishkovea@gmail.com>
2025-06-18 18:28:01 +0300

    init микросервис встреч

---
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/VeloxaMeetingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.morrs.veloxameetingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxaMeetingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxaMeetingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ veloxa-meeting-microservice/src/test/java/com/morrs/veloxameetingmicroservice/VeloxaMeetingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrs.veloxameetingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxaMeetingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
