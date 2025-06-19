4d4b226ea522cb9076c667dcb3968dc4344a9bae
morrs <grishkovea@gmail.com>
2025-06-19 21:55:21 +0300

    setup: добавил зависимости для кафки

--- veloxa-meeting-microservice/build.gradle.kts
+++ veloxa-meeting-microservice/build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
