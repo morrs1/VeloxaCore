73bde778550029efa887cae73879b785cbeaa80e
morrs <grishkovea@gmail.com>
2025-06-19 21:54:26 +0300

    fix: добавил пустой конструктор

--- veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/entity/Meeting.java
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/entity/Meeting.java
@@ -2,12 +2,14 @@ package com.morrs.veloxameetingmicroservice.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Meeting {
 
     UUID id;
--- veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/entity/User.java
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/entity/User.java
@@ -2,11 +2,13 @@ package com.morrs.veloxameetingmicroservice.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private UUID id;
     private String surname;
