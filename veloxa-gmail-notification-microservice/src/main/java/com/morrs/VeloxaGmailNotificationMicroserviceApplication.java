21fc6e43b0fed97ae22ea8dc787d6dd60fd8af52
morrs <grishkovea@gmail.com>
2025-06-19 20:17:15 +0300

    init notification microservice

---
+++ veloxa-gmail-notification-microservice/src/main/java/com/morrs/VeloxaGmailNotificationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.morrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxaGmailNotificationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxaGmailNotificationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ veloxa-gmail-notification-microservice/src/test/java/com/morrs/VeloxaGmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxaGmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
