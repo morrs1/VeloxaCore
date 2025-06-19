0391f6be4f942a45e7cfa613b0788c290f1bd8ef
morrs <grishkovea@gmail.com>
2025-06-19 21:59:28 +0300

    feat: добавил сущности для приема сообщений из кафки

---
+++ veloxa-gmail-notification-microservice/src/main/java/com/morrs/dto/UsersNotification.java
@@ -0,0 +1,17 @@
+package com.morrs.dto;
+
+import com.morrs.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersNotification {
+    private UUID id;
+    private List<User> users;
+}
