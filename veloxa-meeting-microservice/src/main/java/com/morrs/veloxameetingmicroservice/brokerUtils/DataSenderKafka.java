69606df0d110dba3cc5a9f7a7e5c0519921a5018
morrs <grishkovea@gmail.com>
2025-06-19 21:57:07 +0300

    feat: добавил реализацию интерфейса для отправки сообщений

---
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/brokerUtils/DataSenderKafka.java
@@ -0,0 +1,36 @@
+package com.morrs.veloxameetingmicroservice.brokerUtils;
+
+import com.morrs.veloxameetingmicroservice.dto.UsersNotification;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@AllArgsConstructor
+public class DataSenderKafka implements DataSender {
+
+    private String topic;
+    private KafkaTemplate<String, UsersNotification> kafkaTemplate;
+
+
+    @Override
+    public void send(UsersNotification usersNotification) {
+        try {
+            log.info("value:{}", usersNotification);
+            kafkaTemplate.send(topic, usersNotification)
+                    .whenComplete((result, e) -> {
+                        if (e == null) {
+                            log.info(
+                                    "message id:{} was sent, offset:{}",
+                                    usersNotification.getId(),
+                                    result.getRecordMetadata().offset()
+                            );
+                        } else {
+                            log.error("message id:{} was not sent", usersNotification.getId(), e);
+                        }
+                    });
+        } catch (Exception ex) {
+            log.error("send error, value:{}", usersNotification, ex);
+        }
+    }
+}
