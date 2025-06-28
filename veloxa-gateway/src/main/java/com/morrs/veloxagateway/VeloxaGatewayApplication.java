da5437abd67ee0432e775a3ea557109d9515c74e
morrs <grishkovea@gmail.com>
2025-06-28 20:28:33 +0300

    init: gateway сервис

---
+++ veloxa-gateway/src/main/java/com/morrs/veloxagateway/VeloxaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.morrs.veloxagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxaGatewayApplication.class, args);
+	}
+
+}
---
+++ veloxa-gateway/src/test/java/com/morrs/veloxagateway/VeloxaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrs.veloxagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
