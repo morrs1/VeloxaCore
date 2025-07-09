fbfcdfa193adf058d6c3429d8ef6f44bc3475c16
morrs <grishkovea@gmail.com>
2025-07-09 17:46:38 +0300

    refactor: добавил миграции для бд пользователей

--- veloxa-user-microservice/db/liquibase/changelog/v.1.0.0/init.sql
+++ veloxa-user-microservice/db/liquibase/changelog/v.1.0.0/init.sql
@@ -1,6 +1,21 @@
 --liquibase formatted sql
 
+--changeset morrs:1.1
 
+CREATE TABLE users
+(
+    id                 UUID PRIMARY KEY,
+    surname            VARCHAR(255) NOT NULL,
+    name               VARCHAR(255) NOT NULL,
+    patronymic         VARCHAR(255),
+    user_status        VARCHAR(255),
+    notification_way   VARCHAR(255) CHECK (notification_way IN ('GMAIL', 'TELEGRAM')),
+    role               VARCHAR CHECK (role IN ('ADMIN', 'USER')),
+    email              VARCHAR(255),
+    password           VARCHAR,
+    telegram_id        INT,
+    profile_photo_link VARCHAR
+);
 
 
 
