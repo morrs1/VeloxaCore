58a9ed0f5c2ea2841076c9eed6d8485d8fdefc81
morrs <grishkovea@gmail.com>
2025-07-12 17:45:47 +0300

    refactor: добавил ошибку валидации

---
+++ veloxa-meeting-microservice/src/main/java/com/morrs/veloxameetingmicroservice/domain/exceptions/ValidationException.java
@@ -0,0 +1,8 @@
+package com.morrs.veloxameetingmicroservice.domain.exceptions;
+
+public class ValidationException extends BaseException {
+	public ValidationException(String message) {
+		super(message);
+		this.statusCode = 422;
+	}
+}
